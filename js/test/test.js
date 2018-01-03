680a1a92e9d374ee9497a85e59e58569c26bbc00
Andrei Lapets <a@lapets.io>
2018-01-02 20:23:03 -0500

    Fix test script; update package version.

--- js/test/test.js
+++ js/test/test.js
@@ -1,6 +1,6 @@
 var assert = require('assert');
 var expect = require('chai').expect;
-var imparse = require('../lib/imparse');
+var imparse = require('../src/imparse');
 
 describe('imparse', function() {
 
