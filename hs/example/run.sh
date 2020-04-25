6489c82c2608043f46bae9e108730733298c3206
Andrei Lapets <a@lapets.io>
2020-04-24 23:59:44 -0400

    Remove legacy versions (moved to imparse-legacy).

--- hs/example/run.sh
+++
@@ -1,3 +0,0 @@
-imparse -html -hs "" "example.p"
-ghc --make example.hs
-example "example.eg"
--- py/release/release.sh
+++
@@ -1,5 +0,0 @@
-## Release script for PyPI.
-
-python setup.py sdist bdist_wininst register upload
-
-##eof
